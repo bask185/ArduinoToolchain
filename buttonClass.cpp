b572381a63d65b9868f82b2bffb3d1ec97d851f0
bask185 <sknippels@hollandmechanics.com>
2019-11-26 11:37:38 +0100

    modified buttonRead

--- buttonClass.cpp
+++ buttonClass.cpp
@@ -1,17 +1,36 @@
-unsigned char readButton(unsigned char _button) {
-	if(!debounceTime) {
-		debounceTime = 20;
-		// for all buttons
-		
-		if(_button != statePrev) { // if a change occured
-			statePrev = _button;
+#include <Arduino.h>
+#include "buttonClass.h"
 
-			if(_button)	return RISING; 
+Button::Button(unsigned char _pin) {
+	pinMode(_button, INPUT_PULLUP);
+	pin = _pin; }
+
+Button::unsigned char readButton() {
+	byte retValue = state;
+
+	if(state == RISING) state = RELEASED; // take note I use a pull-up resistor
+	if(state == FALLING)state = PRESSED; // rising or falling may be returned just once
+
+	return retValue; }
+
+Button::unsigned char updateButton() {
+	static bool oldSample = false, statePrev = false;
+	bool newSample = digitalRead(pin);
+
+	if(newSample == oldSample) {	// if the same state is detected atleast twice in 20ms...
+	
+		if(newSample != statePrev) { // if a flank change occured return RISING or FALLING
+			statePrev = newSample;
+
+			if(newSample)	return RISING; 
 			else			return FALLING; }
 
-		else {							// if there is no change
-		
-			if(_button)	return PRESSED; 
-			else			return RELEASED; } } }
+		else {						// or if there is no flank change return PRESSED or RELEASED
+			if(newSample)	return PRESSED; 
+			else			return RELEASED; } }
+
+	oldSample = newSample; }
+
+
 
 
