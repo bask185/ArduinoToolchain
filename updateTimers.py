7048be9d1d1d376b68924aa1a2d34ade2f083e91
bask185 <sebastiaan_knippels4@hotmail.com>
2019-12-25 15:00:59 +0100

    used timer 1, ISR functions flawlessly now

--- updateTimers.py
+++ updateTimers.py
@@ -22,18 +22,21 @@ with open("timers.cpp", "w") as scheduler:  #scheduler.c
     scheduler.write('#include "scheduler.h"\n\n')
     scheduler.write("extern void initTimers() {")
     scheduler.write("""
-	TCCR2B = 0;// same for TCCR2B
-	TCNT2  = 0;//initialize counter value to 0
-	// set compare match register for 8khz increments
-	OCR2A = 249;// = (16*10^6) / (1000*64) - 1 (must be <256)
-	// turn on CTC mode
-	TCCR2A |= (1 << WGM21);
-	// Set CS21and and CS20 bit for 64 prescaler
-	TCCR2B |= (1 << CS20);
-	TCCR2B |= (1 << CS21); 
-	TCCR2B &= ~(1 << CS22); 
-	// enable timer compare interrupt
-	TIMSK2 |= (1 << OCIE2A); }
+	cli();//stop interrupts
+    //set timer1 interrupt at 1kHz
+    TCCR1A = 0;// set entire TCCR1A register to 0
+    TCCR1B = 0;// same for TCCR1B
+    TCNT1  = 0;//initialize counter value to 0
+    // set timer count for 1khz increments
+    OCR1A = 1999;// = (16*10^6) / (1000*8) - 1
+    //had to use 16 bit timer1 for this bc 1999>255, but could switch to timers 0 or 2 with larger prescaler
+    // turn on CTC mode
+    TCCR1B |= (1 << WGM12);
+    // Set CS11 bit for 8 prescaler
+    TCCR1B |= (1 << CS11);  
+    // enable timer compare interrupt
+    TIMSK1 |= (1 << OCIE1A);
+    sei();//allow interrupts
 
 """)
 
@@ -42,7 +45,7 @@ with open("timers.cpp", "w") as scheduler:  #scheduler.c
 
     scheduler.write("""
 // Don't complain about the indentations. This code is generated for you and you shouldn't be looking at this part.
-ISR(TIMER2_OVF_vect) {
+ISR(TIMER1_COMPA_vect) {
 static unsigned char _1ms, _10ms, _100ms, _1000ms;
 
 _1ms += 1; 
