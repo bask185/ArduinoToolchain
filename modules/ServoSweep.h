a9dc8d6562bc544496b07103b39447e240018d4f
bask185 <sknippels@hollandmechanics.com>
2020-08-27 16:39:23 +0200

    The servo sweep modules is improved. But for some strange reason it won't compile yet. All variables are not 'declared in this scope' in the constructor. The header is included and it remains a mystery

--- modules/ServoSweep.h
+++ modules/ServoSweep.h
@@ -1,20 +1,16 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-class {
+class ServoSweep {
+
 public:
-    ServoSweep();
-    sweep();
+    ServoSweep( byte _servoPin, byte _inputPin, byte _min, byte _max, byte _speed  ) ;        // constructor 1
+    //ServoSweep( byte _servoPin, byte _inputPin, byte _min, byte _max, byte _speed, _relayPin ) ;  // constructor 2
+    void sweep();
 
 private:
-    enum servoStates {
-        moveLeft,
-        moveRight,
-    } ;
-
     Servo servo ;
     unsigned long timeToRun ;
-    byte servoPin ;
     byte inputPin ;
     byte state ;
     byte pos ;
@@ -23,4 +19,9 @@ private:
     byte servoSpeed ;
     byte servoMin ;
     byte servoMax  ;
+    byte middlePosition ;
+    byte relayPresent ;
+    byte relayPin ;
+
 } ;
+
--- modules/servoSweep.cpp
+++ modules/servoSweep.cpp
@@ -1,25 +1,60 @@
-#include "servoSweep.h"
 
 
-void ServoSweep( byte _pin, byte _input, byte _min, byte _max, byte _speed ) {
+// enum servoStates {
+//     movingLeft,
+//     movingRight,
+// } ;
+
+#include "ServoSweep.h"
+
+#define movingLeft 0
+#define movingRight 1
+
+void ServoSweep( byte _servoPin, byte _inputPin, byte _min, byte _max, byte _speed ) {                   // constructor 1
     
-    servoPin = _pin ;
-    inputPin = _input ;
+    servoPin = _servoPin ;
+    inputPin = _inputPin ;
     servoSpeed = _speed ;
     servoMin = _min ;
     servoMax = _max ;
 
-    state = movingLeft ;
+    if( digitalRead( inputPin ) == true ) state = movingLeft ;
+    else                                  state = movingRight ;
+    
+    middlePosition = ( (long)servoMax - (long)servoMin ) / (long)2 + (long)servoMin ;               // start with middle position
 
-    pos = ( servoMax - servoMin ) / 2 + servoMin ; // start with middle position
+    pos = middlePosition
 
     pinMode( inputPin, INPUT_PULLUP ) ;
     servo.write( pos ) ;
     servo.attach( servoPin ) ;
 }
 
+void ServoSweep( byte _servoPin, byte _inputPin, byte _min, byte _max, byte _speed, byte _relayPin ) {      // constructor 2
+    
+    servoPin = _servoPin ;
+    inputPin = _inputPin ;
+    servoSpeed = _speed ;
+    servoMin = _min ;
+    servoMax = _max ;
 
-void ServoSweep::sweep ( byte state ) {
+    if( digitalRead( inputPin ) == true ) state = movingLeft ;
+    else                                  state = movingRight ;
+
+    middlePosition = ( (long)servoMax - (long)servoMin ) / (long)2 + (long)servoMin ;
+
+    pos = middlePosition
+
+    pinMode( inputPin, INPUT_PULLUP ) ;
+    servo.write( pos ) ;
+    servo.attach( servoPin ) ;
+
+    relayPresent = 1;
+    relayPin = _relayPin ;
+}
+
+
+void ServoSweep::sweep ( ) {
     unsigned long currentTime = millis() ;
 
     if( currentTime > timeToRun ) {
@@ -27,20 +62,25 @@ void ServoSweep::sweep ( byte state ) {
 
         switch( state ) {
 
-        case moveRight:
+        case movingRight:
             if( pos < servoMax ) pos ++ ;
-            if( digitalRead( inputPin ) ) state = moveLeft ;
+            if( digitalRead( inputPin ) ) state = movingLeft ;
             break ;
 
-        case moveLeft:
+        case movingLeft:
             if( pos > servoMin ) pos -- ;
-            if( !digitalRead( inputPin ) ) state = moveRight ;
+            if( !digitalRead( inputPin ) ) state = movingRight ;
             break ;
         }
 
         if( prevPos != pos ) {
             prevPos  = pos ;
-           
+
+            if( relayPresent == 1 ) {
+                if( pos < middlePosition ) digitalWrite( relayPin,  LOW ) ;
+                else                       digitalWrite( relayPin, HIGH ) ;
+            }
+
             servo.write( pos ) ;
         }
     }
