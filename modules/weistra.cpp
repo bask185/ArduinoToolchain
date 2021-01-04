97828c83dfb44376c7c02d969720a2fb08e87914
bask185 <sknippels@hollandmechanics.com>
2021-01-04 08:03:04 +0100

    Modified the Weistra controller, there was a bug which could cause a jump of a loco while turning down the dutycycle. With thanks to Hennik of BNLS this has been  resolved. I also added 2 macros for a minimal and maximum freqency. It turned out that not every loco responded well to 20Hz

--- modules/weistra.cpp
+++ modules/weistra.cpp
@@ -1,5 +1,8 @@
 #include "weistra.h"
 
+#define Fmax 100
+#define Fmin 50
+
 Weistra::Weistra(unsigned char _pin) { // constructor
     trackPin = _pin;
 } 
@@ -14,8 +17,8 @@ void Weistra::begin() {
 }
 
 void Weistra::update() {
-    // static byte counter = 0;
-    // static uint32_t prevTime = 0;
+    static byte counter = 0;
+    static uint32_t prevTime = 0;
 
     if( portx_p != 0 ) {
         uint32_t currentTime = micros()/* & 0x0000FFFF*/; // we only use the last 2 bytes of micros()
@@ -27,7 +30,7 @@ void Weistra::update() {
             if( counter == 0 && dutyCycle > 0) {  // if counter reaches 100, reset it to 0 and enable the track power pin
                 *portx_p |=  trackPin;
             }
-            if( counter == dutyCycle && dutyCycle < 100) {
+            if( counter >= dutyCycle && dutyCycle < 100) {
                 *portx_p &= ~trackPin;
             }
             counter++;
@@ -42,8 +45,8 @@ void Weistra::setSpeed(byte speed) {
 
     dutyCycle = constrain(speed, 0, 100); // speed limit = 0 - 100
 
-    if( dutyCycle <= 10 ) { frequency = 20; }
-    else                  { frequency = map(dutyCycle, 10, 100, 20, 100); }
+    if( dutyCycle <= 10 ) { frequency = Fmin; }
+    else                  { frequency = map(dutyCycle, 10, 100, Fmin, Fmax); }
 
     intervalTime = 10000 / frequency; // > between 100us and 500us
  }
