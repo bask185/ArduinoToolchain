9e4049fbb24133fa1d7c7cca757717d9555d56fb
bask185 <sebastiaan_knippels4@hotmail.com>
2021-11-22 09:17:37 +0100

    Added more quality of life macros. Also added a commented line with #debug. The state machine header now includes macros.h and examines the debug flag to print or not to print the state names

--- src/macros.h
+++ src/macros.h
@@ -2,6 +2,8 @@
 #ifndef MACROS
 #define MACROS
 
+// #define DEBUG
+
 #define REPEAT_US(x)    { \
                             static uint32_t previousTime ;\
                             uint32_t currentTime = micros() ;\
@@ -16,6 +18,30 @@
                                 // code to be repeated goes between these 2 macros
 #define END_REPEAT          } \
                         }
+
+#define printNumber( txt, x )   Serial.print( txt ) ; Serial.print( x ) ;
+#define printNumber_( txt, x )  Serial.print( txt ) ; Serial.print( x ) ;Serial.write(' ');
+#define printNumberln( txt, x ) Serial.print( txt ) ; Serial.println( x ) ;
+
+#define lowByte(x)          ((x) & 0x00FF )
+#define highByte(x)         (((X) & 0xFF00 ) > 8)
+
+#define INT8                int8_t
+#define UINT8               uint8_t
+#define INT16               int16_t
+#define UINT16              uint6_t
+#define INT32               int32_t
+#define UINT32              uint32_t
+
+#define SET(x,y)            ((x) |=  (1 << (y)))
+#define CLR(x,y)            ((x) &= ~(1 << (y)))
+#define TGL(x,y)            ((x) ^=  (1 << (y)))
+#define READ(x,y)           (((x) >> (y)) & 1)
+
+
+#define STRING char*
+#define STR2(s)             #s
+#define STR(s)              STR2(s)
 
 
 #endif
--- src/stateMachineClass.h
+++ src/stateMachineClass.h
@@ -1,42 +1,40 @@
 #include <Arduino.h>
+#include "macros.h"
 
 #define StateFunction( x ) bool x##F()
-#define State(x) break; case x: /*if(sm.runOnce) Serial.println(#x);*/ if(x##F())
+#ifndef DEBGUG
+#define State(x) break; case x: if(x##F())
+#else   
+#define State(x) break; case x: if(sm.runOnce) Serial.println(#x); if(x##F())   // if debug is defined, all states are stringerized and printed when entry state is run
+#endif
 #define STATE_MACHINE_BEGIN if( sm.run() ) switch( sm.getState() ) {
 #define STATE_MACHINE_END break ; } return sm.getState() ;
-
-enum states 
-{
-    stateX,
-    stateY,
-    stateZ,
-} ;
 
 class StateMachine {
 public:
     StateMachine() ;
     
-    void setState( uint8_t ) ;
+    void    setState( uint8_t ) ;
     uint8_t getState() ;
-    void nextState( uint8_t, uint32_t ) ;
+    void    nextState( uint8_t, uint32_t ) ;
     uint8_t entryState() ;
     uint8_t onState() ;
     uint8_t exitState() ;
     uint8_t run() ;
     
-    void setTimeout( uint32_t ) ;
+    void    setTimeout( uint32_t ) ;
     uint8_t timeout() ;
-    void exit() ;
-    void reboot() ;
+    void    exit() ;
+    void    reboot() ;
     uint8_t endState() ;
     
 private:
-    uint8_t runOnce ;
-    uint8_t enabled ;
-    uint8_t exitFlag ;
+    uint8_t  runOnce ;
+    uint8_t  enabled ;
+    uint8_t  exitFlag ;
     uint32_t prevTime ;
     uint32_t interval ;
-    uint8_t state;
+    uint8_t  state;
 } ;
 
 
