64984923df7db1b6dac1c125beb611f521ee7e30
bask185 <sebastiaan_knippels4@hotmail.com>
2023-08-07 10:27:52 +0200

    Altered servoSweep library - servo min may be bigger than servo max, (relay direction must be added) - servo is now attached within the sweep method - EEPROM may be used by calling useEEPROM( eeAddress, flags). min and max positions are stored in EEPROM - Default values of constructor will be loaded if EEPROM is not-initialized/corrupted - Active servo state may also be stored if the STORE_POSITION flag is set. This will be used upon a reset to set the servo to the correct position
    TODO:
    - relay direction needs compensation when servo min is larger than max. Good use for the ? operator?
    - code is untested.
    - some doxygen docu may be nice for the constructors and eeprom init function.

--- modules/ServoSweep.h
+++ modules/ServoSweep.h
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+const int STORE_POSITIONS   = 0b00000001 ;
+const int DEFAULT_BIT       = 0b10000000 ;
+
 class ServoSweep {
 
 public:
@@ -8,9 +11,10 @@ public:
     ServoSweep( uint8_t _servoPin, uint8_t _min, uint8_t _max, uint8_t _speed, uint8_t _turnOff, uint8_t _relayPin ) ;  // constructor 2
     uint8_t sweep( );
     void setState( uint8_t _state );
-    // void turnOn() ;
-    // void turnOff() ;
     void begin( );
+    void useEEPROM( uint16_t, uint8_t ) ;
+    void setMin( uint8_t _min) ;
+    void setMax( uint8_t _max) ;
 
 private:
     Servo servo ;
@@ -23,8 +27,9 @@ private:
     uint8_t servoMin ;
     uint8_t servoMax  ;
     uint8_t middlePosition ;
-    uint8_t relayPresent ;
     uint8_t relayPin ;
     uint8_t turnOff ;
+    uint8_t eeFlags ;
+    uint16_t eeAddress = 0xFFFF ;
     unsigned long currentTime = millis() ;
 } ;
--- modules/servoSweep.cpp
+++ modules/servoSweep.cpp
@@ -3,6 +3,7 @@
 #define movingLeft 0
 #define movingRight 1
 
+
 ServoSweep::ServoSweep( uint8_t _servoPin, uint8_t _min, uint8_t _max, uint8_t _turnOff,  uint8_t _speed ) {                   // constructor 1
     
     servoPin = _servoPin ;
@@ -11,7 +12,8 @@ ServoSweep::ServoSweep( uint8_t _servoPin, uint8_t _min, uint8_t _max, uint8_t _
     servoMax = _max ;
    
     middlePosition = ( (long)servoMax - (long)servoMin ) / (long)2 + (long)servoMin ;               // start with middle position
-    pos = middlePosition ;
+
+    relayPin = 0xFF ; // no relay
 
     if( _turnOff ) turnOff = 1 ;
     else           turnOff = 0 ;
@@ -26,63 +28,106 @@ ServoSweep::ServoSweep( uint8_t _servoPin, uint8_t _min, uint8_t _max, uint8_t _
 
     middlePosition = ( (long)servoMax - (long)servoMin ) / (long)2 + (long)servoMin ;
 
-    pos = middlePosition ;
-
-    relayPresent = 1;
     relayPin = _relayPin ;
 
     if( _turnOff ) turnOff = 1 ;
     else           turnOff = 0 ;
 }
 
-void ServoSweep::begin() {
-    if( servoPin != 255) {
-        servo.write( pos ) ;
-        servo.attach( servoPin ) ;
+void ServoSweep::begin()
+{
+    if( eeAddress != 0xFFFF ) // If EEPROM present
+    {
+        if( eeFlags & DEFAULT_BIT ) // if this bit is set, we must initialize the EEPROM
+        {
+            EEPROM.update( eeAddress+0, servoMin ) ;
+            EEPROM.update( eeAddress+1, servoMax ) ;
+            EEPROM.update( eeAddress+2,        0 ) ; // last state
+        }
+
+        servoMin = EEPROM.read( eeAddress+0 ) ;
+        servoMax = EEPROM.read( eeAddress+1 ) ;
+        middlePosition = ( (long)servoMax - (long)servoMin ) / (long)2 + (long)servoMin ;
+
+        if( eeFlags & STORE_POSITIONS )
+        {
+            state = EEPROM.read(eeAddress+2) ;
+        }
     }
 
-    if( relayPresent ) pinMode( relayPin, OUTPUT ) ;
+    if( relayPin != 0xFF ) pinMode( relayPin, OUTPUT ) ;
 }
 
-void ServoSweep::setState( uint8_t _state ) {
+void ServoSweep::setState( uint8_t _state )
+{
     state = _state ;
     if( turnOff ) servo.attach( servoPin ) ;
+
+    if( eeFlags & STORE_POSITIONS )
+    {
+        EEPROM.update( eeAddress+2, state ) ;
+    }
 }
 
-// void ServoSweep::turnOn() {
-//     servo.attach( servoPin ) ;
-// }
+void ServoSweep::setMin( uint8_t _min)
+{
+    servoMin = _min ;
+    EEPROM.update( eeAddress+0, servoMin ) ;
+}
 
-// void ServoSweep::turnOff() {
-//     servo.detach(  ) ;
-// }
+void ServoSweep::setMax( uint8_t _max)
+{
+    servoMax = _max ;
+    EEPROM.update( eeAddress+1, servoMax ) ;
+}
 
-uint8_t ServoSweep::sweep ( ) {
-    if( millis() > timeToRun ) {
-        timeToRun = millis() + servoSpeed ;
+uint8_t ServoSweep::sweep ( )
+{
+    if( millis() - lastTime > servoSpeed )
+    {       lastTime = millis() ;
+     
+        uint8_t setPoint ;
 
-        if( state ) {
-            if( pos < servoMax ) pos ++ ;
-        }
-        else {
-            if( pos > servoMin ) pos -- ;
-        }
+        if( state ) setPoint = servoMax ; // get set point
+        else        setPoint = servoMin ;
 
+        if( pos < setPoint ) pos ++ ;   // follow positon to setpoint
+        if( pos > setPoint ) pos -- ;
 
-        if( prevPos != pos ) {
+        if( prevPos != pos ) {          // if position has changed.
             prevPos  = pos ;
 
-            if( ( pos == servoMax || pos == servoMin ) && turnOff == 1 ) servo.detach( ) ;
+            if( servoPin != 255 ) servo.write( pos ) ;
 
-            if( relayPresent ) {
+            if( pos != setPoint 
+            &&  servo.attached() == false ) // attach motor if needed
+            {
+                servo.attach( servoPin ) ;
+            }
+
+            if( pos == setPoint 
+            &&  turnOff == 1 )
+            {
+                servo.detach( ) ; // detach motor if needed
+            }
+
+            if( relayPin != 0xFF ) // SK: todo, add something that relay does not change direction
+            {                      //           when servo min is smaller than max
                 if( pos < middlePosition ) digitalWrite( relayPin,  LOW ) ;
                 else                       digitalWrite( relayPin, HIGH ) ;
             }
-            if( servoPin != 255 ) servo.write( pos ) ;
+            
             return pos ;
         }
-        else {
+        else
+        {
             return 0 ;
         }
     }
+}
+
+void ServoSweep::useEEPROM( uint16_t _eeAddress, uint8_t _eeFlags )
+{
+    eeAddress = _eeAddress ;
+    eeFlags = _eeFlags ;
 }
