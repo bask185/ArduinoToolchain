6faa1364cfc74bbfd681e2dc7e1078a0a880e2d1
bask185 <sebastiaan_knippels4@hotmail.com>
2024-01-08 13:40:04 +0100

    updated weistra and re-added nmra dcc library.
    Weistra code has now built in current sensing. Can be use if desired

--- modules/weistra.cpp
+++ modules/weistra.cpp
@@ -3,49 +3,118 @@
 
 Weistra::Weistra(uint8_t _pin, uint8_t _Fmin, uint8_t _Fmax ) 
 { // constructor
-    trackPin = _pin;
+    trackPin1 = _pin;
+    trackPin2 = 255 ;
     Fmin = _Fmin ;
     Fmax = _Fmax ;
+    sensePin = 255 ;
+}
+
+Weistra::Weistra(uint8_t _pin1, uint8_t _pin2, uint8_t _Fmin, uint8_t _Fmax ) 
+{ // constructor
+    trackPin1 = _pin1;
+    trackPin2 = _pin2;
+    Fmin = _Fmin ;
+    Fmax = _Fmax ;
+    sensePin = 255 ;
 } 
+
 
 void Weistra::begin()
 {
-    pinMode(trackPin, OUTPUT);
+    pinMode(trackPin1, OUTPUT);
+    pinMode(trackPin2, OUTPUT);
 
-    byte port   = digitalPinToPort( trackPin );
-    trackPin    = digitalPinToBitMask( trackPin );
-    portx_p     = portOutputRegister( port );
+    uint8_t port1 = digitalPinToPort( trackPin1 );
+    trackPin1     = digitalPinToBitMask( trackPin1 );
+    portx_p1      = portOutputRegister( port1 );
+
+    if( trackPin2 != 255 )
+    {
+        uint8_t port2 = digitalPinToPort( trackPin2 );
+        trackPin2     = digitalPinToBitMask( trackPin2 );
+        portx_p2      = portOutputRegister( port2 );
+    }
+
+    state = 1 ;
+}
+
+/*currentLimit = Imax * Rshunt / 0.00488 */
+void Weistra::setCurrentSense( uint8_t _sensePin, uint8_t _currentLimit )
+{
+    sensePin = _sensePin ;
+    currentLimit = _currentLimit ;
 }
 
 void Weistra::update() 
 {
-    if( portx_p != 0 )
+    if( portx_p1 || portx_p2 )
     {
         uint32_t currentTime = micros() ; 
 
         if( currentTime - prevTime >= intervalTime )
-        { 
-            prevTime = currentTime;
+        {   prevTime = currentTime;
 
-            if( counter == 0 && newDutyCycle > 0 )      // if counter reaches 100, reset it to 0 and enable the track power pin
+            if( !state )
             {
-                *portx_p |=  trackPin ;
+                *portx_p1 &= ~trackPin1;
+                *portx_p2 &= ~trackPin2;
+            }
+            else if( counter == 0 && newDutyCycle > 0 )      // if counter reaches 100, reset it to 0 and enable the track power pin
+            {
+                if(dir) *portx_p1 |=  trackPin1 ;
+                else    *portx_p2 |=  trackPin2 ;
+
                 dutyCycle = newDutyCycle ;              // a new dutycucle can only be accepted on the beginning of a cycle, this prevents weird jumps of the trains
                 intervalTime = newIntervalTime ;        // new speed is accepted at the beginning of a cycle
             }
             if( counter == dutyCycle /*&& dutyCycle < 100*/ ) // commented code seems buggy??
             {
-                *portx_p &= ~trackPin;
+                *portx_p1 &= ~trackPin1;
+                *portx_p2 &= ~trackPin2;
             }
             if( ++counter > 100) counter = 0 ;
+        }
+
+        if( sensePin != 255 )
+        {
+            int current = analogRead( sensePin ) ;
+
+            if( millis() - prevTime2 > interval 
+            && (*portx_p1 & trackPin1 || *portx_p2 & trackPin2 || !state ) )
+            {     prevTime2 = millis() ;
+
+                if( state ) // if on
+                {
+                    if( current > currentLimit ) currentCounter -- ;
+                    else                         currentCounter = 5 ;
+
+                    if( currentCounter == 0 )
+                    {
+                        setState( 0 ) ;
+                        interval = 5000 ; // 5s power off
+                    }
+                }
+                else        // if off
+                {
+                    setState( 1 ) ; // turn on again
+                    interval = sampleRate ; // set sample rate 
+                    currentCounter = 2 ;
+                }
+            }
         }
     }
 }
 
 
-void Weistra::setSpeed(byte speed)
+void Weistra::setSpeed( int8_t speed)
 {
     byte frequency;
+    speedBackup = speed ;
+    if( speed < 0 ) dir = 1 ; // 1 is reverse
+    if( speed > 0 ) dir = 0 ;
+
+    speed = abs( speed ) ;
 
     newDutyCycle = constrain( speed, 0, 100 ) ; // speed limit = 0 - 100
 
@@ -55,17 +124,23 @@ void Weistra::setSpeed(byte speed)
     newIntervalTime = 10000 / frequency; // > between 100us and 500us
 }
 
+int8_t Weistra::getSpeed()
+{
+    return speedBackup ;
+}
+
 void Weistra::setState( uint8_t _state )
 { 
     state = _state ;
     
-    if( !state )
+    if( state )
     {
-        *portx_p &= ~trackPin ;
+        counter = 0 ;
     }
     else
     {
-        counter = 0 ;
+        *portx_p1 &= ~trackPin1 ;
+        *portx_p2 &= ~trackPin2 ;
     }
 }
 
--- modules/weistra.h
+++ modules/weistra.h
@@ -3,24 +3,38 @@
 class Weistra {
 public:
     Weistra(uint8_t, uint8_t, uint8_t);
+    Weistra(uint8_t, uint8_t, uint8_t, uint8_t);
     void begin();
     void update();
-    void setSpeed(uint8_t);
+    void setSpeed(int8_t);
+    int8_t getSpeed();
     void setState(uint8_t ) ;
+    void setCurrentSense( uint8_t, uint8_t ) ;
     uint8_t getState( ) ;
 
 private:
-    uint16_t intervalTime ;
+    uint32_t intervalTime ;
     uint16_t newIntervalTime ;
-    uint8_t trackPin ;
+    uint16_t interval ;
+    uint32_t prevTime ;
+    uint8_t trackPin1 ;
+    uint8_t trackPin2 ;
     uint8_t dutyCycle ;
     uint8_t newDutyCycle ;
     uint8_t Fmin ;
     uint8_t Fmax ;
     uint8_t state ;
+    uint8_t dir ;
+    uint8_t currentLimit ;
+    uint8_t sensePin ;
+    int8_t  speedBackup ;
     uint8_t counter = 0;
-    uint32_t prevTime = 0;
+    uint8_t currentCounter = 0;
+    uint32_t prevTime2 = 0;
 
-    volatile uint8_t *portx_p ;
+    const int sampleRate = 10 ;
+
+    volatile uint8_t *portx_p1 ;
+    volatile uint8_t *portx_p2 ;
 };
 
